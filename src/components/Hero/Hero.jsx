81290caa85c2aab2efc7c9016bf0275485a922d0
Aqib An Nafi <aqib_an_nafi@yahoo.com>
2026-03-02 01:01:12 +0600

    hero done

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 
-
+import HeroImg from "../../assets/banner-img.png"
 const Hero = () => {
     return (
         <div>
@@ -10,14 +10,14 @@ const Hero = () => {
                 <div className='mb-5'>
                     <p className='text-center'>Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.</p>
                 </div>
-                <div className='flex px-48 gap-5'>
+                <div className='flex px-48 gap-5 mb-5'>
                     <input type="text" placeholder="Search any doctor" className="input flex-2 rounded-full" />
                     <button className="btn btn-primary rounded-full">Search Now</button>
                 </div>
             </div>
-            <div>
-                <img src="" alt="" />
-                <img src="" alt="" />
+            <div className="flex gap-5">
+                <img src={HeroImg} alt="" />
+                <img src={HeroImg} alt="" />
             </div>
         </div>
     );
